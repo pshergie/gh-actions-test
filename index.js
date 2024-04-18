50ce1f984c25495d323300e187babeca383b0728
Maksim Minko <dethroll@gmail.com>
2024-04-18 17:41:58 +0200

    test workflow 12

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ async function run() {
     const github_token = core.getInput('GITHUB_TOKEN');
 
     const context = github.context;
+
     if (context.payload.pull_request == null) {
       core.setFailed('No pull request found.');
       return;
     }
+
     const pull_request_number = context.payload.pull_request.number;
 
-    const octokit = new github.GitHub(github_token);
+    // const octokit = new github.GitHub(github_token);
+    const myToken = core.getInput('myToken');
+    const octokit = github.getOctokit(myToken)
     const new_comment = octokit.issues.createComment({
       ...context.repo,
       issue_number: pull_request_number,
       body: message
     });
-
   } catch (error) {
     core.setFailed(error.message);
   }
