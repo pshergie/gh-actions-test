e5a54fb24dc4c5f5b30ee55030b1ff3eb179887a
Maksim Minko <dethroll@gmail.com>
2024-04-22 17:17:51 +0200

    test 17

--- dist/index.js
+++ dist/index.js
@@ -31083,6 +31083,8 @@ const github = __nccwpck_require__(8555);
 
 async function run() {
   try {
+    const messageShort = core.getInput('messageShort');
+    const message = core.getInput('message');
     const myToken = core.getInput('myToken');
     const octokit = github.getOctokit(myToken);
     const context = github.context;
@@ -31093,7 +31095,7 @@ async function run() {
       issue_number: pull_number,
     });
 
-    const isCommentExisting = Array.isArray(comments) && comments.some(comment => comment.user.login === 'github-actions[bot]' && comment.body.includes('Solar System Exploration, 1950s – 1960s'))
+    const isCommentExisting = Array.isArray(comments) && comments.some(comment => comment.user.login === 'github-actions[bot]' && comment.body.includes(messageShort))
 
     const { data } = await octokit.rest.pulls.listFiles({
       ...context.repo,
@@ -31103,19 +31105,11 @@ async function run() {
     const listOfFiles = data.map(change => change.filename);
 
     listOfFiles.map(async (file) => {
-      if (file.includes('src/components') && !isCommentExisting) {
+      if (file.includes('src/components') && !!isCommentExisting) {
         await octokit.rest.issues.createComment({
           ...context.repo,
           issue_number: pull_number,
-          body:
-            `# Solar System Exploration, 1950s – 1960s
-
-- [ ] Mercury
-- [x] Venus
-- [x] Earth (Orbit/Moon)
-- [x] Mars
-- [ ] Jupiter
-- [ ] Saturn`,
+          body: message,
         });
       } else if (file.includes('src/utils') && !isCommentExisting) {
         await octokit.rest.issues.createComment({
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const github = require('@actions/github');
 
 async function run() {
   try {
+    const messageShort = core.getInput('messageShort');
+    const message = core.getInput('message');
     const myToken = core.getInput('myToken');
     const octokit = github.getOctokit(myToken);
     const context = github.context;
@@ -13,7 +15,7 @@ async function run() {
       issue_number: pull_number,
     });
 
-    const isCommentExisting = Array.isArray(comments) && comments.some(comment => comment.user.login === 'github-actions[bot]' && comment.body.includes('Solar System Exploration, 1950s – 1960s'))
+    const isCommentExisting = Array.isArray(comments) && comments.some(comment => comment.user.login === 'github-actions[bot]' && comment.body.includes(messageShort))
 
     const { data } = await octokit.rest.pulls.listFiles({
       ...context.repo,
@@ -23,19 +25,11 @@ async function run() {
     const listOfFiles = data.map(change => change.filename);
 
     listOfFiles.map(async (file) => {
-      if (file.includes('src/components') && !isCommentExisting) {
+      if (file.includes('src/components') && !!isCommentExisting) {
         await octokit.rest.issues.createComment({
           ...context.repo,
           issue_number: pull_number,
-          body:
-            `# Solar System Exploration, 1950s – 1960s
-
-- [ ] Mercury
-- [x] Venus
-- [x] Earth (Orbit/Moon)
-- [x] Mars
-- [ ] Jupiter
-- [ ] Saturn`,
+          body: message,
         });
       } else if (file.includes('src/utils') && !isCommentExisting) {
         await octokit.rest.issues.createComment({
