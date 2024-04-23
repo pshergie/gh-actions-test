74360ac830d5a9cc6a8089d39b4d89198c2cee61
Maksim Minko <dethroll@gmail.com>
2024-04-23 12:09:01 +0200

    fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function run() {
   try {
-    const messageComponents = core.getInput('messageComponents');
-    const messageUtils = core.getInput('messageUtils');
-    const myToken = core.getInput('myToken');
+    const messageComponents = core.getInput("messageComponents");
+    const messageUtils = core.getInput("messageUtils");
+    const myToken = core.getInput("myToken");
     const octokit = github.getOctokit(myToken);
     const context = github.context;
     const pull_number = context.payload.pull_request.number;
@@ -15,29 +15,39 @@ async function run() {
       issue_number: pull_number,
     });
 
-    const isComponentsCommentExisting = !!comments.find(comment => comment.user.login === 'github-actions[bot]' && comment.body === messageComponents);
-    const isUtilsCommentExisting = !!comments.find(comment => comment.user.login === 'github-actions[bot]' && comment.body === messageComponents)
+    const isComponentsCommentExisting = !!comments.find(
+      (comment) =>
+        comment.user.login === "github-actions[bot]" &&
+        comment.body === messageComponents,
+    );
+    const isUtilsCommentExisting = !!comments.find(
+      (comment) =>
+        comment.user.login === "github-actions[bot]" &&
+        comment.body === messageComponents,
+    );
 
     const { data } = await octokit.rest.pulls.listFiles({
       ...context.repo,
       pull_number,
     });
 
-    data.map(change => change.filename).map(async (file) => {
-      if (file.includes('src/components') && !isComponentsCommentExisting) {
-        await octokit.rest.issues.createComment({
-          ...context.repo,
-          issue_number: pull_number,
-          body: message,
-        });
-      } else if (file.includes('src/utils') && !isUtilsCommentExisting) {
-        await octokit.rest.issues.createComment({
-          ...context.repo,
-          issue_number: pull_number,
-          body: messageUtils,
-        });
-      }
-    })
+    data
+      .map((change) => change.filename)
+      .map(async (file) => {
+        if (file.includes("src/components") && !isComponentsCommentExisting) {
+          await octokit.rest.issues.createComment({
+            ...context.repo,
+            issue_number: pull_number,
+            body: messageComponents,
+          });
+        } else if (file.includes("src/utils") && !isUtilsCommentExisting) {
+          await octokit.rest.issues.createComment({
+            ...context.repo,
+            issue_number: pull_number,
+            body: messageUtils,
+          });
+        }
+      });
   } catch (error) {
     core.setFailed(error.message);
   }
