393e10fbbfe4deefbfc7ed8983e5a8da90b7e2dc
Maksim Minko <dethroll@gmail.com>
2024-04-29 11:19:41 +0200

    test 2

--- dist/index.js
+++ dist/index.js
@@ -31086,6 +31086,7 @@ async function run() {
   try {
     const paths = core.getInput("paths");
     console.log("PATHS", paths);
+    console.log(typeof paths);
     const myToken = core.getInput("myToken");
     const octokit = github.getOctokit(myToken);
     const context = github.context;
